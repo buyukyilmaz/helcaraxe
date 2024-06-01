503a0b30a9eba8f27f03331a1078af7d308ca7f0
Uğur Büyükyılmaz <ugur.buyukyilmaz@gmail.com>
2024-06-01 22:47:02 +0300

    1.0.4 released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     kotlin("android")
     id("org.jmailen.kotlinter")
+    `maven-publish`
 }
 
 android {
@@ -21,7 +22,7 @@ android {
     libraryVariants.all {
         outputs.all {
             val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
-            output.outputFileName = "helcaraxe-1.0.3.aar"
+            output.outputFileName = "helcaraxe-1.0.4.aar"
         }
     }
 }
@@ -32,4 +33,17 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register("release", MavenPublication::class) {
+                from(components["release"])
+                groupId = "com.glorfindel.helcaraxe"
+                artifactId = "helcaraxe"
+                version = "1.0.4"
+            }
+        }
+    }
 }
